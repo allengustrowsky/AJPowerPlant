2ea4f249a217116384bdde82c286644e5a24f441
Allen Gustrowsky <allen@may21.us>
2022-12-01 22:31:07 -0500

    Router architecture set up

---
+++ components/Reactor.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+
+const Reactor = (props) => {
+    return (
+        <div>
+            <p style={{color: 'white'}}>Reactor component</p>
+        </div>
+    )
+}
+
+export default Reactor
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import App from './App'
+import Reactor from '../components/Reactor'
 import './index.css'
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <App/>
+    },
+    {
+        path: '/:id',
+        element: <Reactor/>
+    }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 )
