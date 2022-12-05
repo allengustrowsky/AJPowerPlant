8adf446cbd596afd8976811493cc3e807ea26fa9
Allen Gustrowsky <allen@may21.us>
2022-12-05 16:38:39 -0500

    Action button coloring

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import ReactorCard from '../components/ReactorCard'
 // import Paper from '@mui/material/Paper'
 import { Paper, Typography, ButtonGroup, Button } from '@mui/material';
+import { createTheme, ThemeProvider } from '@mui/material/styles'
 
 function App() {
     const [reactorData, setReactorData] = useState('')
@@ -90,6 +91,17 @@ function App() {
           }
     }, [])
 
+    // Theme override for actions button
+    const btnTheme = createTheme({
+        palette: {
+            yellow: {
+                main: '#E0FF4F',
+            },
+            lightRed: {
+                main: '#FF6663',
+            },
+        },
+    })
 
 
     return (
@@ -115,17 +127,19 @@ function App() {
                         Total Output: <p className='totalOutputData'>{(totalOutput === '') ? 'Loading...' : `${totalOutput} Gw`}</p>
                     </Typography>
                     <div className="btnGroup">
-                        <div className="coolantContainer btnCol">
-                            {/* <ButtonGroup className='coolantBtnGroup' orientation='vertical' size='large' variant='outlined'> */}
-                            <Button className='actionBtn' variant='outlined'>Enable Coolant</Button>
-                            <Button className='actionBtn' variant='outlined'>Disable Coolant</Button>
-                            {/* </ButtonGroup> */}
-                        </div>
-                        <div className="btnCol">
-                            <Button className='actionBtn' variant='outlined'>Global Reset</Button>
-                            <Button className='actionBtn' variant='outlined'>Controller Shutdown</Button>
-                        </div>
-                        <Button className='actionBtn' variant='outlined'>Emergency Shutdown</Button>
+                        <ThemeProvider theme={btnTheme}>
+                            <div className="coolantContainer btnCol">
+                                {/* <ButtonGroup className='coolantBtnGroup' orientation='vertical' size='large' variant='outlined'> */}
+                                <Button className='actionBtn' variant='contained' color='yellow'>Enable Coolant</Button>
+                                <Button className='actionBtn' variant='contained' color='yellow'>Disable Coolant</Button>
+                                {/* </ButtonGroup> */}
+                            </div>
+                            <div className="btnCol">
+                                <Button className='actionBtn' variant='contained' color='yellow'>Global Reset</Button>
+                                <Button className='actionBtn' variant='contained' color='yellow'>Controller Shutdown</Button>
+                            </div>
+                            <Button className='actionBtn emergencyBtn' variant='contained' color='lightRed'>Emergency Shutdown</Button>
+                        </ThemeProvider>
                     </div>
 
                 </Paper>
