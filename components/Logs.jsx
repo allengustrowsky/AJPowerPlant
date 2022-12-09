f2c89c77b47b796e06db86beda3a171ad73a7f99
Allen Gustrowsky <allen@may21.us>
2022-12-09 13:54:45 -0500

    Log polling working

--- components/Logs.jsx
+++ components/Logs.jsx
@@ -12,56 +12,71 @@ const Logs = (props) => {
         enqueueSnackbar('my snackbar')
     }
 
-    useEffect(() => {
-        // Get messages
-        const getMessages = async () => {
-            const raw = await fetch('https://nuclear.dacoder.io/reactors/logs?apiKey=' + apiKey)
-            const jsonData = await raw.json()
-            setLogs(jsonData)
-        }
-        getMessages()
-    }, [])
-
-    const getMessages = () => {
-        if (logs !== '') {
-            const msgs = logs.reduce((reactorMessages, reactorLog) => { // log object from list of logs
-                for (const reactorId in reactorLog) { // id of reactor in log object
-                    reactorLog[reactorId].forEach(message => { // record each message inside individual log object
-                        reactorMessages.push(message)
-                    })
-                }
-                console.log('reduce')
-                return reactorMessages
-            }, [])
-            if (msgs !== messages) {
-                console.log('new messages!')
-                console.log('old: ' + messages)
-                console.log('new: ' + msgs)
-                setMessages(msgs)
-                // Update snackbar
-                msgs.forEach(message => {
-                    enqueueSnackbar(message, { 
-                        action, 
-                        persist: true, 
-                        preventDuplicate: true,
-                        style: {
-                            width: '350px',
-                            textAlign: 'left',
-                        },
-                    })
+    const getLogs = async () => {
+        const raw = await fetch('https://nuclear.dacoder.io/reactors/logs?apiKey=' + apiKey)
+        const jsonData = await raw.json()
+        setLogs(jsonData)
+        const msgs = jsonData.reduce((reactorMessages, reactorLog) => { // log object from list of logs
+            for (const reactorId in reactorLog) { // id of reactor in log object
+                reactorLog[reactorId].forEach(message => { // record each message inside individual log object
+                    reactorMessages.push(message)
                 })
-
-            } else {
-                console.log('same old msgs')
             }
-
-        }
+            return reactorMessages
+        }, [])
+        setMessages(msgs)
     }
 
-    // Extract individual messages after logs have been retrieved
     useEffect(() => {
+        // Get messages
+        getLogs()
+        const id = setInterval(getLogs, 200)
+
+        return () => {
+            clearInterval(id)
+        }
+    }, [])
+
+    // const getMessages = () => {
+    //     if (logs !== '') {
+    //         const msgs = logs.reduce((reactorMessages, reactorLog) => { // log object from list of logs
+    //             for (const reactorId in reactorLog) { // id of reactor in log object
+    //                 reactorLog[reactorId].forEach(message => { // record each message inside individual log object
+    //                     reactorMessages.push(message)
+    //                 })
+    //             }
+    //             console.log('reduce')
+    //             return reactorMessages
+    //         }, [])
+    //         if (msgs !== messages) {
+    //             console.log('new messages!')
+    //             console.log('old: ' + messages)
+    //             console.log('new: ' + msgs)
+    //             setMessages(msgs)
+    //             // Update snackbar
+    //             // msgs.forEach(message => {
+    //             //     enqueueSnackbar(message, { 
+    //             //         action, 
+    //             //         persist: true, 
+    //             //         preventDuplicate: true,
+    //             //         style: {
+    //             //             width: '350px',
+    //             //             textAlign: 'left',
+    //             //         },
+    //             //     })
+    //             // })
+
+    //         } else {
+    //             console.log('same old msgs')
+    //         }
+
+    //     }
+    // }
+
+    // Extract individual messages after logs have been retrieved
+    // useEffect(() => {
         // const fetchMessages
-        getMessages()
+        // getMessages()
         // const msgIntervalId = setInterval(getMessages, 10000)
 
         // delete interval to prevent memory leak on component unmount
@@ -69,15 +84,13 @@ const Logs = (props) => {
             // clearInterval(msgIntervalId)
         // }
 
-    }, [logs])  
+    // }, [logs])  
 
     return (
         <Card className='logsContainer' sx={{width: '40rem', height: '20rem', backgroundColor: 'var(--dark-blue)', color: 'var(--white)', overflow: 'scroll'}}>
-            <Button onClick={handleClick}>Click me</Button>
-            <Typography variant='h4' component='h2'>Reactor Logs</Typography>
-            {messages === '' ? 'Loading...' : messages.map(message => {
-                console.log('rerender')
-                return <Card className='message'>{message}</Card>
+            <Typography variant='h4' component='h2'>System Logs</Typography>
+            {messages === '' ? 'Loading...' : messages.map((message, index) => {
+                return <Typography key={index} variant='subtitle1' component='p'>{message}</Typography>
             })
     }
         </Card>
